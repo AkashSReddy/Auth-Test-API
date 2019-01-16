8f91891b9b20efbfbe766700652076b5ac841750
AkashSReddy <akashsreddy98@gmail.com>
2019-01-16 12:03:36 +0530

    routes added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const passport = require("passport");
+const database = require("../services/adminfunction");
+const auth = require("../middleware/authentication");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.json("Auth-Test-API");
 });
 
+router.get("/route", (req, res, next) => {
+  try {
+    return res.redirect("/user");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/register", async (req, res, next) => {
+  try {
+    let message = await database.addUser(req.body);
+    console.log(message);
+    if (message === "ok") {
+      return res.send("register is Success");
+    }
+    res.json(message);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+router.post(
+  "/login",
+  passport.authenticate("login", {
+    successRedirect: "/route",
+    failureRedirect: "/",
+    failureFlash: true
+  })
+);
 module.exports = router;
